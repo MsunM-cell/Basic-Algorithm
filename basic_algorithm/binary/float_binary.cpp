6916a6342ecf258092a37c2f4852b0e68bcf72c2
MsunM-cell <2464831035@qq.com>
2021-08-08 23:13:01 +0800

    binary

---
+++ basic_algorithm/binary/float_binary.cpp
@@ -0,0 +1,25 @@
+// 2020-08-08
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    scanf("%lf", &x);
+
+    double l = 0, r = x;
+    while (r - l > 1e-8)
+    {
+        double mid = (l + r) / 2;
+        if (mid * mid >= x)
+            r = mid;
+        else
+            l = mid;
+    }
+
+    printf("%lf", l);
+
+    return 0;
+}
---
+++ basic_algorithm/binary/integer_binary.cpp
@@ -0,0 +1,49 @@
+// 2020-08-08
+
+#include <iostream>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+
+int n, q, k;
+int a[N];
+
+int main()
+{
+    scanf("%d%d", &n, &q);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    
+    while (q--)
+    {
+        scanf("%d", &k);
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            int mid = l + r >> 1;
+            if (a[mid] >= k)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        if (a[l] != k)
+            printf("-1 -1\n");
+        else
+        {
+            printf("%d ", l);
+            l = 0, r = n - 1;
+            while (l < r)
+            {
+                int mid = l + r + 1 >> 1;
+                if (a[mid] <= k)
+                    l = mid;
+                else
+                    r = mid - 1;
+            }
+            printf("%d\n", l);
+        }
+    }
+
+    return 0;
+}
