73ffb29399a7e61042d54e6cf90ed9a767dfea40
MsunM-cell <2464831035@qq.com>
2021-08-09 18:14:28 +0800

    high_precision_subtract

---
+++ basic_algorithm/high_precision/subtract.cpp
@@ -0,0 +1,67 @@
+// 2020-08-09
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool cmp(vector<int> &A, vector<int> &B)
+{
+    if (A.size() != B.size())
+        return A.size() > B.size();
+    
+    for (int i = A.size() - 1; i >= 0; i--)
+        if (A[i] != B[i])
+            return A[i] > B[i];
+    
+    return true;
+}
+
+vector<int> subtract(vector<int> &A, vector<int> &B)
+{
+    vector<int> C;
+    for (int i = 0, t = 0; i < A.size(); i++)
+    {
+        t = A[i] - t;
+        if (i < B.size()) 
+            t -= B[i];
+        C.push_back((t + 10) % 10);
+        if (t < 0)
+            t = 1;
+        else 
+            t = 0;
+    }
+
+    while (C.size() > 1 && C.back() == 0)
+        C.pop_back();
+    
+    return C;
+}
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    vector<int> A, B;
+    for (int i = a.size() - 1; i >= 0; i--)
+        A.push_back(a[i] - '0');
+    for (int i = b.size() - 1; i >= 0; i--)
+        B.push_back(b[i] - '0');
+    
+    if (cmp(A, B))
+    {
+        auto C = subtract(A, B);
+        for (int i = C.size() - 1; i >= 0; i--)
+            cout << C[i];
+    }
+    else 
+    {
+        auto C = subtract(B, A);
+        cout << "-";
+        for (int i = C.size() - 1; i >= 0; i--)
+            cout << C[i];
+    }
+
+    return 0;
+}
